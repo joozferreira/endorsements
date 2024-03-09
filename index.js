54d99e78e2bad622d2ec2be66af9906468c127d5
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2024-03-09 22:20:36 +0000

    Changes to understand dupes when adding endorsement

--- index.js
+++ index.js
@@ -14,18 +14,20 @@ const appSettings = {
     "https://endorsements-b3db0-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
+// Variables to create and link to new database
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const endorsementListInDB = ref(database, "endorsementList");
 
+// Global variables
 const publishBtn = document.getElementById("publish");
 const textEndorsement = document.getElementById("endorsement-text");
 const fromEndorsement = document.getElementById("from");
 const toEndorsement = document.getElementById("to");
-const likesEndorsement = document.getElementById("likes");
 const endorsementSection = document.getElementById("endorsement-section");
 let endorsementDetails = {};
 
+// Capture of endorsement from user and push to DB
 publishBtn.addEventListener("click", function () {
   endorsementDetails = {
     text: textEndorsement.value,
@@ -35,25 +37,41 @@ publishBtn.addEventListener("click", function () {
   };
 
   push(endorsementListInDB, endorsementDetails);
-  createEndorsement(endorsementDetails);
+
   clearInputFields();
 });
 
+// Update of endorsement list anytime any change/new endorsement is created
 onValue(endorsementListInDB, function (snapshot) {
   if (snapshot.exists()) {
     const endorsementArr = Object.values(snapshot.val());
-    endorsementArr.forEach((item) => createEndorsement(item));
+
+    clearEndorsementSection();
+
+    console.log(endorsementArr);
+
+    endorsementArr.forEach((item) => {
+      console.log(item);
+      createEndorsement(item);
+    });
   } else {
     endorsementSection.innerHTML = "There are no endorsements yet...";
   }
 });
 
+// Clearing of input fields after submission
 function clearInputFields() {
   textEndorsement.value = "";
   fromEndorsement.value = "";
   toEndorsement.value = "";
 }
 
+// Clearing of endorsement section to ensure no endorsement is repeated
+function clearEndorsementSection() {
+  endorsementSection.innerHTML = "";
+}
+
+// Use of template to create new endorsement cards
 function createEndorsement(endorsement) {
   if ("content" in document.createElement("template")) {
     const template = document.getElementById("endorsement-template");
